ad11d8672019426cf539b9782586b738912a9326
EmekaAgara <emekaagara@gmail.com>
2022-04-29 15:54:22 +0100

    authentication

--- firebase.js
+++ firebase.js
@@ -1,17 +1,25 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import * as firebase from "firebase";
+import "firebase/firestore";
+import "firebasae/auth";
 
-// Your web app's Firebase configuration
+
 const firebaseConfig = {
   apiKey: "AIzaSyAfGrUGeqv162mLProji1WUJYZerw0t3K8",
   authDomain: "chat-app-3fb1b.firebaseapp.com",
   projectId: "chat-app-3fb1b",
   storageBucket: "chat-app-3fb1b.appspot.com",
   messagingSenderId: "788077091301",
-  appId: "1:788077091301:web:e15a097a0bc6979050770c"
+  appId: "1:788077091301:web:e15a097a0bc6979050770c",
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
+let app;
+
+if (firebase.apps.length === 0){
+  app = firebase.initializeApp(firebaseConfig);
+} else {
+  app = firebase.app();
+}
+
+
+const db = ap.firestore();
+const auth = firebase.auth();
